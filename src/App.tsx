691652dad0da5dec3f7bd0aaa8c50db363246fc8
ToxicCreator <aleks7000bol@gmail.com>
2022-09-10 00:17:03 +0300

    fix App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
-import WeekTabs from './components/week-tabs/WeekTabs';
-import logo from './logo.svg';
-import './App.css';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import WeekTabs from 'components/week-tabs/WeekTabs';
+import {LessonCard, type Lesson} from 'components/lesson-card';
+
+
+const lessons = [
+  {
+    name: "кр. 17 н. Алгоритмы компонентов цифровой обработки данных",
+    type: "лек",
+    groups: ["БСБО-01-19", "БСБО-02-19", "БСБО-03-19", "ББСО-01-19", "ББСО-02-19"],
+    place: "251"
+  },
+  {},
+  {
+    name: "Технологическая (проектно-технологическая) практика",
+    type: "лаб",
+    groups: ["БСБО-01-19", "БСБО-02-19"],
+  },
+  {
+    name: "кр. 17 н. Алгоритмы компонентов цифровой обработки данных",
+    type: "пр",
+    place: "301",
+    groups: ["БСБО-01-19"],
+  },
+  {
+    name: "кр. 17 н. Алгоритмы компонентов цифровой обработки данных",
+    type: "пр",
+    groups: ["БСБО-01-19"],
+  },
+  {}
+]
 
 function App() {
   return (
     <div className="App">
       <WeekTabs />
+      <Stack>
+        {
+          lessons.map((lesson, lessonNumber) => {
+            return (
+              <LessonCard
+                key={lessonNumber}
+                lessonNumber={lessonNumber}
+                {...lesson}
+              />
+            );
+          })
+        }
+      </Stack>
     </div>
   );
 }
