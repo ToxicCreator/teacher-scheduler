73e5d17b8c8c936fb35315aaf21a64a46e9289ec
ToxicCreator <aleks7000bol@gmail.com>
2022-09-24 19:33:01 +0300

    add DayScreen

---
+++ src/screens/DayScreen.tsx
@@ -0,0 +1,57 @@
+import Stack from '@mui/material/Stack';
+import WeekTabs from 'components/week-tabs/WeekTabs';
+import {LessonCard, type Lesson} from 'components/lesson-card';
+import Screen from './Screen';
+
+
+const lessons: Array<Lesson | {}> = [
+  {
+    name: "кр. 17 н. Алгоритмы компонентов цифровой обработки данных",
+    type: "лек",
+    groups: ["БСБО-01-19", "БСБО-02-19", "БСБО-03-19", "ББСО-01-19", "ББСО-02-19"],
+    place: "251"
+  },
+  {},
+  {
+    name: "Технологическая (проектно-технологическая) практика",
+    type: "лаб",
+    groups: ["БСБО-01-19", "БСБО-02-19"],
+  },
+  {
+    name: "кр. 17 н. Алгоритмы компонентов цифровой обработки данных",
+    type: "пр",
+    place: "301",
+    groups: ["БСБО-01-19"],
+  },
+  {
+    name: "кр. 17 н. Алгоритмы компонентов цифровой обработки данных",
+    type: "пр",
+    groups: ["БСБО-01-19"],
+  },
+  {}
+];
+
+const lessonsMap = (
+  lesson: Lesson | {}, 
+  lessonNumber: number
+) => {
+  return (
+    <LessonCard
+      key={lessonNumber}
+      lessonNumber={lessonNumber}
+      {...lesson}
+    />
+  );
+}
+
+function DayScreen() {
+  return (
+    <Screen header={<WeekTabs />} >
+      <Stack>
+        {lessons.map(lessonsMap)}
+      </Stack>
+    </Screen>
+  );
+}
+
+export default DayScreen;
